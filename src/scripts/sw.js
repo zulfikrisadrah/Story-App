f4154cc2bab379e67ad883b70b4f003abc240208
zulfikrisadrah <muhzulfikri26@gmail.com>
2025-05-12 15:05:35 +0800

    deploy

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -2,24 +2,6 @@ import { precacheAndRoute } from 'workbox-precaching';
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-const CACHE_NAME = 'pwa-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/app.css',
-  '/images/logo.png',
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    }),
-  );
-  self.skipWaiting();
-});
-
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
